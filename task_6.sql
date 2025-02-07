c1a2c7aa7bf43e7fc8be1bdfa204fded24e77f91
James Audu <ayomideyolasunkanmi25@gmail.com>
2025-02-07 14:29:45 -0800

    Added correct code to task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,8 +1,8 @@
 -- task_6.sql
 
--- Insert multiple rows into the customer table
-INSERT INTO customer (customer_id, customer_name, email, address)
+-- Insert multiple rows into the customer table without specifying customer_id
+INSERT INTO customer (customer_name, email, address)
 VALUES
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+    ('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+    ('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    ('Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
