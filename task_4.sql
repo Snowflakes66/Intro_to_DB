18ba79b417fe2c13a2e41d103d2f8054a6b81f6a
James Audu <ayomideyolasunkanmi25@gmail.com>
2025-02-07 14:04:30 -0800

    Added correct text to task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,15 @@
+-- task_4.sql
+
+-- Query to get the full description of the books table
+SELECT
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM
+    information_schema.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
